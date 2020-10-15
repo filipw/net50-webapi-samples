0017ac16b687799ab3f148f7b14a7c6ca3e61322
filipw <wojcieszyn@gmail.com>
2020-10-15 16:34:19 +0200

    added simplest v2

---
+++ src/01-b/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+// simplest variant 2
+WebHost.Start(routes => routes.MapGet("hello/{name}", (req, res, data) => res.WriteAsync($"Hello, {data.Values["name"]}")));
+Console.ReadKey();
--- src/01/Program.cs
+++ src/01/Program.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 // simplest
 WebHost.CreateDefaultBuilder().Configure(app => app.Run(c => c.Response.WriteAsync("Hello world!"))).Build().Run();
