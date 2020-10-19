d6644749f75b39d60ecda7aff3b44d0118a09675
filipw <wojcieszyn@gmail.com>
2020-10-19 16:59:42 +0200

    simplified demo with records

--- src/03/Contacts.cs
+++ src/03/Contacts.cs
@@ -3,62 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-public class Contact
-{
-    public int ContactId { get; set; }
+public record Contact(int ContactId, string Name, string Address, string City);
 
-    public string Name { get; set; }
-
-    public string Address { get; set; }
-
-    public string City { get; set; }
-}
-
-public class InMemoryContactRepository
+public class ContactService
 {
     private readonly List<Contact> _contacts = new List<Contact>
         {
-            new Contact { ContactId = 1, Name = "Filip W", Address = "Bahnhofstrasse 1", City = "Zurich" },
-            new Contact { ContactId = 2, Name = "Josh Donaldson", Address = "1 Blue Jays Way", City = "Toronto" },
-            new Contact { ContactId = 3, Name = "Aaron Sanchez", Address = "1 Blue Jays Way", City = "Toronto" },
-            new Contact { ContactId = 4, Name = "Jose Bautista", Address = "1 Blue Jays Way", City = "Toronto" },
-            new Contact { ContactId = 5, Name = "Edwin Encarnacion", Address = "1 Blue Jays Way", City = "Toronto" }
+            new Contact(1, "Filip W", "Bahnhofstrasse 1", "Zurich"),
+            new Contact(2, "Josh Donaldson", "1 Blue Jays Way", "Toronto"),
+            new Contact(3, "Aaron Sanchez", "1 Blue Jays Way", "Toronto"),
+            new Contact(4, "Jose Bautista", "1 Blue Jays Way", "Toronto"),
+            new Contact(5, "Edwin Encarnacion", "1 Blue Jays Way", "Toronto")
         };
 
-    public Task<IEnumerable<Contact>> GetAll()
-    {
-        return Task.FromResult(_contacts.AsEnumerable());
-    }
+    public Task<IEnumerable<Contact>> GetAll() => Task.FromResult(_contacts.AsEnumerable());
 
-    public Task<Contact> Get(int id)
-    {
-        return Task.FromResult(_contacts.FirstOrDefault(x => x.ContactId == id));
-    }
+    public Task<Contact> Get(int id) => Task.FromResult(_contacts.FirstOrDefault(x => x.ContactId == id));
 
     public Task<int> Add(Contact contact)
     {
         var newId = (_contacts.LastOrDefault()?.ContactId ?? 0) + 1;
-        contact.ContactId = newId;
-        _contacts.Add(contact);
+        _contacts.Add(new Contact(newId, contact.Name, contact.Address, contact.City));
         return Task.FromResult(newId);
-    }
-
-    public async Task Update(Contact updatedContact)
-    {
-        var contact = await Get(updatedContact.ContactId).ConfigureAwait(false);
-        if (contact == null)
-        {
-            throw new InvalidOperationException(string.Format("Contact with id '{0}' does not exists", updatedContact.ContactId));
-        }
-
-        contact.Address = updatedContact.Address;
-        contact.City = updatedContact.City;
-        contact.Name = updatedContact.Name;
     }
 
     public async Task Delete(int id)
     {
-        var contact = await Get(id).ConfigureAwait(false);
+        var contact = await Get(id);
         if (contact == null)
         {
             throw new InvalidOperationException(string.Format("Contact with id '{0}' does not exists", id));
--- src/03/Program.cs
+++ src/03/Program.cs
@@ -7,26 +7,25 @@ using Microsoft.Extensions.DependencyInjection;
 // with endpoint routing and JSON endpoints
 // see https://github.com/dotnet/aspnetcore/pull/23496/ for more on the JSON helpers
 WebHost.CreateDefaultBuilder().
-ConfigureServices(s => s.AddSingleton<InMemoryContactRepository>()).
+ConfigureServices(s => s.AddSingleton<ContactService>()).
 Configure(app => 
 {
     app.UseRouting();
     app.UseEndpoints(e => 
     {
+        var contactService = e.ServiceProvider.GetRequiredService<ContactService>();
         e.MapGet("/contacts", async c => 
-            await c.Response.WriteAsJsonAsync(await c.RequestServices.GetRequiredService<InMemoryContactRepository>().GetAll()));
+            await c.Response.WriteAsJsonAsync(await contactService.GetAll()));
         e.MapGet("/contacts/{id:int}", async c => 
-            await c.Response.WriteAsJsonAsync(await c.RequestServices.GetRequiredService<InMemoryContactRepository>().Get(int.Parse((string)c.Request.RouteValues["id"]))));
+            await c.Response.WriteAsJsonAsync(await contactService.Get(int.Parse((string)c.Request.RouteValues["id"]))));
         e.MapPost("/contacts", async c => 
         {
-            var repo = c.RequestServices.GetRequiredService<InMemoryContactRepository>();
-            await repo.Add(await c.Request.ReadFromJsonAsync<Contact>());
+            await contactService.Add(await c.Request.ReadFromJsonAsync<Contact>());
             c.Response.StatusCode = 201;
         });
         e.MapDelete("/contacts/{id:int}", async c => 
         {
-            var repo = c.RequestServices.GetRequiredService<InMemoryContactRepository>();
-            await repo.Delete(int.Parse((string)c.Request.RouteValues["id"]));
+            await contactService.Delete(int.Parse((string)c.Request.RouteValues["id"]));
             c.Response.StatusCode = 204;
         });
     });
